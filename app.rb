1d7c18a803458c0dae4ddde5c79ec507efdf0928
Michael Martinez <zeroeclipse@live.com>
2021-01-20 19:59:05 -0800

    commit tree 4ac2d210f43ed0ee3c4c423b37dd5c4a4c3fccc3

--- app.rb
+++ app.rb
@@ -1,6 +1,41 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get "/reversename/:name" do
+    "#{params[:name].reverse}"
+  end
 
+  get "/square/:number" do
+    square = params[:number].to_i * params[:number].to_i
+    "#{square}"
+  end
+
+  get "/say/:number/:phrase" do
+    string = ""
+    phrase = params[:phrase]
+    params[:number].to_i.times {|i| string += phrase}
+    "#{string}"
+  end
+
+  get "/say/:word/:word2/:word3/:word4/:word5" do
+    string = "#{params[:word]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    "#{string}"
+  end
+
+  get "/:operation/:number1/:number2" do
+    if params[:operation] == 'add'
+      string = params[:number1].to_i + params[:number2].to_i
+      return string.to_s
+    elsif params[:operation] == 'subtract'
+      string = params[:number1].to_i - params[:number2].to_i
+      return string.to_s
+    elsif params[:operation] == 'multiply'
+      string = params[:number1].to_i * params[:number2].to_i
+      return string.to_s
+    elsif params[:operation] == 'divide'
+      string = params[:number1].to_i / params[:number2].to_i
+      return string.to_s
+    end
+  end
 end
